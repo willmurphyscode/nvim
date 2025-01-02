9d8cafdaa61e5a8e5d4b3dfdb8533a35115cb6fd
Will Murphy <willmurphyscode@users.noreply.github.com>
2025-01-02 07:51:08 -0500

    add python pack

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.test.neotest" },
   -- import/override with your plugins folder
 }
